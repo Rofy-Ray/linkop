a957dbb990120788a0c8a2cff90a60d51c889679
Rofy-Ray <okyereforsonr@berea.edu>
2024-02-21 12:42:44 -0600

    cleanup default profile image render

--- linkop/settings.py
+++ linkop/settings.py
@@ -34,8 +34,8 @@ load_dotenv(BASE_DIR / "..env")
 SECRET_KEY = os.getenv('SECRET_KEY', os.getenv('DEFAULT_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'RENDER' not in os.environ
-DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
+DEBUG = 'RENDER' not in os.environ
+# DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(' ')
